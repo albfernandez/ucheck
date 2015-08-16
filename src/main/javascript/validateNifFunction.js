781bd5397637b30bde155007a38f202995d70d46
alberto <infjaf@gmail.com>
2015-08-16 23:15:22 +0200

    Remove struts related stuff

--- src/main/javascript/validateNifFunction.js
+++ src/main/javascript/validateNifFunction.js
@@ -109,40 +109,4 @@
 		return (_validateNif(value) || _validateNie(value) || _validateCif(value));
 	}
 	
-	/**
-	 * Comprueba si los campos son NIFs válidos.
-	 * Los campos no se revisan si están desactivados o sin valor.
-	 * <p>
-	 * @param form El formulario que se está verificando.
-	 */
-	function validateNif(form){
-		var isValid = true;
-		var focusField = null;
-		var i = 0;
-		var fields = new Array();
-		var formName = form.getAttributeNode("name");
-		
-		oNif = eval('new ' + formName.value + '_nif()');
-		for (x in oNif) {
-			var field = form[oNif[x][0]];
-			if ((field.type == 'hidden' ||
-				field.type == 'text' ||
-				field.type == 'password' ||
-				field.type == 'textarea') &&
-				(field.value.length > 0) &&
-				field.disabled == false) {
-				if (!_validateNif(field.value)){
-					if (i == 0) {
-						focusField = field;
-					}
-					fields[i++] = oNif[x][1];
-					isValid = false;
-				}
-			}
-		}
-		if (fields.length > 0) {
-			focusField.focus();
-			alert(fields.join('\n'));
-		}
-		return isValid;
-	}
+
